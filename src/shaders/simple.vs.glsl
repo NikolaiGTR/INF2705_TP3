9427a7ba4a34ea43ad7067f4ff4aee6ef81d05c3
NikolaiGTR <nikolai.olekhnovitch@polymtl.ca>
2024-02-29 15:47:42 -0500

    Added simple shader

--- src/shaders/simple.vs.glsl
+++ src/shaders/simple.vs.glsl
@@ -5,7 +5,12 @@ layout (location = 2) in vec3 normal;
 
 uniform mat4 mvp;
 
+float growthFactor = 0.1f;
+
 void main()
 {
     // TODO
+    position = position + (normalize(normal) * growthFactor);
+    gl_Position = mvp * vec4(position, 1.0f);
 }
+ 
